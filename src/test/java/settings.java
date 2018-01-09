796ce2ab55fe4c3f72717c551c3fde9048029325
NoiseOfRain <noiseor@gmail.com>
2018-01-09 22:11:38 +0400

    add driver for win

--- src/test/java/settings.java
+++ src/test/java/settings.java
@@ -6,16 +6,30 @@ import java.io.File;
 
 public class settings {
 
-    WebDriver driver;
+    static public WebDriver driver;
+    static private String wayToWebDriver;
 
+    private void getOperationSystem() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")){
+            wayToWebDriver = "src\\test\\resurses\\geckodriverForWindows.exe";
+        }
+        else if (os.contains("osx")){
+            wayToWebDriver = null;
+        }
+        else if (os.contains("nix") || os.contains("aix") || os.contains("nux")){
+            wayToWebDriver = "src/test/resurses/geckodriverForUbuntu";
+        }
+    }
 
     @Test
     public void runTest() {
 
-        System.setProperty("webdriver.gecko.driver", "src/test/resurses/geckodriverForUbuntu");
+        getOperationSystem();
+
+        System.setProperty("webdriver.gecko.driver", wayToWebDriver);
         driver = new FirefoxDriver();
         driver.get("http://ya.ru");
 
     }
-
 }
