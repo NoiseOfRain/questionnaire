61ec2063a8f87684dcebbd01bb2c6d65a91be69f
NoiseOfRain <noiseor@gmail.com>
2018-01-12 17:29:55 +0400

    refactoring screen delete

--- src/test/java/Logging/makeTestLogs.java
+++ src/test/java/Logging/makeTestLogs.java
@@ -3,13 +3,12 @@ package Logging;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 
 import java.io.File;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 
@@ -18,7 +17,7 @@ public class makeTestLogs extends SettingsForTests.settings {
     static int numberOfScreen = 0;
     static String nameOfScreen = "";
 
-    //@AfterMethod
+    @AfterMethod
     public static void getScreenshot() throws IOException, InterruptedException {
 
         //не сразу отрисовываются фото
@@ -42,14 +41,10 @@ public class makeTestLogs extends SettingsForTests.settings {
         File file = new File(filePathFull);
 
         List<File> files = sortFiles(file);
-        /*
-        while (file.listFiles().length > 5) {
-            sortFiles(file);
-        */
 
-        for (File children : files) {
-            for (File x : children.listFiles()) {
-                System.out.println(x);
+        while (files.size() > 5) {
+            for (File children : files.get(0).listFiles()) {
+                children.delete();
             }
         }
     }
